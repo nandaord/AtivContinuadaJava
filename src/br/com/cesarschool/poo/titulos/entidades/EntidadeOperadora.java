60c1f2af745adca74b6ee3b9bf2f363c2f27c83f
Rafa Vidal <142419595+Rafabvidal@users.noreply.github.com>
2024-10-01 09:58:38 -0300

    correcao autorizacao  boolean

--- src/br/com/cesarschool/poo/titulos/entidades/EntidadeOperadora.java
+++ src/br/com/cesarschool/poo/titulos/entidades/EntidadeOperadora.java
@@ -24,11 +24,11 @@ public class EntidadeOperadora {
 
     private long identificador;
     private String nome;
-    private double autorizadoAcao;
+    private boolean autorizadoAcao;
     private double saldoAcao;
     private double saldoTituloDivida;
 
-    public EntidadeOperadora(long identificador, String nome, double autorizadoAcao) {
+    public EntidadeOperadora(long identificador, String nome, boolean autorizadoAcao) {
         this.identificador = identificador;
         this.nome = nome;
         this.autorizadoAcao = autorizadoAcao;
@@ -49,11 +49,11 @@ public class EntidadeOperadora {
         return nome;
     }
 
-    public void setAutorizadoAcao(double autorizadoAcao){
+    public void setAutorizadoAcao(boolean autorizadoAcao){
         this.autorizadoAcao = autorizadoAcao;
     }
 
-    public double getAutorizadoAcao(){
+    public boolean getAutorizadoAcao(){
         return autorizadoAcao;
     }
 
