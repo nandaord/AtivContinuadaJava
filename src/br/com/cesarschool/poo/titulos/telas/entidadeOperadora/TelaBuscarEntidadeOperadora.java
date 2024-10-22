354b75b20e1a8de5766043ca8c2e5932b7fae786
Rafa Vidal <142419595+Rafabvidal@users.noreply.github.com>
2024-10-22 20:40:27 -0300

    Update TelaBuscarEntidadeOperadora.java

--- src/br/com/cesarschool/poo/titulos/telas/entidadeOperadora/TelaBuscarEntidadeOperadora.java
+++ src/br/com/cesarschool/poo/titulos/telas/entidadeOperadora/TelaBuscarEntidadeOperadora.java
@@ -15,6 +15,16 @@ public class TelaBuscarEntidadeOperadora{
     private MediatorEntidadeOperadora mediatorEntidadeOperadora = MediatorEntidadeOperadora.getInstance();
 
     public static void main(String[] args) {
+         try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Não foi possível definir o look and feel.");
+        }
         SwingUtilities.invokeLater(() -> {
             try {
                 TelaBuscarEntidadeOperadora window = new TelaBuscarEntidadeOperadora();
@@ -44,6 +54,7 @@ public class TelaBuscarEntidadeOperadora{
         tela.setBounds(100, 100, 556, 370);
         tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         tela.getContentPane().setLayout(null);
+        tela.getContentPane().setBackground(new java.awt.Color(190, 190, 190));
     }
 
     private void criarComponentes() {
