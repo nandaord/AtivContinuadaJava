10ec87a896cf1eb52da33a674d5a88e8e954455f
Rafa Vidal <142419595+Rafabvidal@users.noreply.github.com>
2024-10-22 20:41:00 -0300

    Update TelaExcluirEntidadeOperadora.java

--- src/br/com/cesarschool/poo/titulos/telas/entidadeOperadora/TelaExcluirEntidadeOperadora.java
+++ src/br/com/cesarschool/poo/titulos/telas/entidadeOperadora/TelaExcluirEntidadeOperadora.java
@@ -22,6 +22,16 @@ public class TelaExcluirEntidadeOperadora {
     private static final int LARGURA_TEXTFIELD = 122;
 
     public static void main(String[] args) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Não foi possível definir o look and feel.");
+        }
         SwingUtilities.invokeLater(() -> {
             try {
                 TelaExcluirEntidadeOperadora window = new TelaExcluirEntidadeOperadora();
@@ -45,6 +55,7 @@ public class TelaExcluirEntidadeOperadora {
         tela.setBounds(100, 100, 556, 370);
         tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         tela.getContentPane().setLayout(null);
+        tela.getContentPane().setBackground(new java.awt.Color(190, 190, 190));
     }
 
     private void initialize() {
