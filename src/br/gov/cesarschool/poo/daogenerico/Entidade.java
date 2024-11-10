2c361a8c8ab1b482d105a4a2d9f16f715d414a5d
Maria Fernanda Ordonho <mfso@cesar.school>
2024-11-09 23:55:25 -0300

    importei as classes novas e a biblioteca do jUnit pros testes

---
+++ src/br/gov/cesarschool/poo/daogenerico/Entidade.java
@@ -0,0 +1,20 @@
+package br.gov.cesarschool.poo.daogenerico;
+/*
+ * Esta classe representa uma superclasse de todas as entidades.
+ *
+ * Deve ter os seguintes atributos (com respectivos set e get):
+ *	LocalDateTime dataHoraInclusao,
+ *  LocalDateTime dataHoraUltimaAlteracao,
+ *	String usuarioInclusao e
+ *	String usuarioUltimaAlteracao
+ *
+ * Deve ter um único construtor sem parâmetros.
+ *
+ * Deve ser abstrata.
+ *
+ * Deve ter um método abstrato getIdUnico().
+ *
+ * Deve implementar a interface Serializable do JAVA
+ */
+public abstract class Entidade {
+}
